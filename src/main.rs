3578d78b96bfd8cdbb6b46168468d85e7766e688
Matheus Fernando Finatti <mf.finatti@gmail.com>
2024-06-22 15:14:54 +0100

    try to drive ST7789 PIM580 display
    at the moment it just displays a blank screen

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,42 @@
 
 use bsp::entry;
 use bsp::hal;
-use defmt_rtt as _;
-use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::OutputPin;
 use hal::pac;
 use panic_halt as _;
 
-use rp_pico::hal::gpio::FunctionUart;
-use rp_pico::hal::uart::*;
-use rp_pico::hal::Clock;
+use hal::gpio::*;
+use hal::Clock;
 
 use rp_pico as bsp;
 
+// Mipidsi drivers
+use mipidsi::*;
+
+// Embedded HAL
+use embedded_hal::digital::OutputPin;
+
+// Embedded graphics to draw on the display
+use embedded_graphics::draw_target::*;
+use embedded_graphics::geometry::*;
+use embedded_graphics::pixelcolor::*;
+use embedded_graphics::primitives::*;
+use embedded_graphics::Drawable;
+
+// Embed the 'Hz' function.
+use fugit::RateExtU32;
+
+use rtt_target::{rprintln, rtt_init_print};
+
+// Display specs
+const WIDTH: i32 = 320;
+const HEIGHT: i32 = 240;
+
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+
+    rprintln!("Starting");
+
     let mut pac = pac::Peripherals::take().unwrap();
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
@@ -42,26 +64,66 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Configure GPIO25 as output
+    // Configure LED pin as output
     let mut led_pin = pins.gpio25.into_push_pull_output();
 
-    // Config UART
-    let uart_pins = (
-        pins.gpio0.into_function::<FunctionUart>(),
-        pins.gpio1.into_function::<FunctionUart>(),
+    // Configure SPI pins for display
+    let spi_mosi: Pin<_, FunctionSpi, PullNone> = pins.gpio19.reconfigure();
+    let spi_sclk: Pin<_, FunctionSpi, PullNone> = pins.gpio18.reconfigure();
+
+    // Init SPI
+    let spi = hal::Spi::<_, _, _, 8>::new(pac.SPI0, (spi_mosi, spi_sclk));
+    let spi = spi.init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        62_500_000.Hz(),
+        &embedded_hal::spi::MODE_0,
     );
 
-    let uart = UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
-        .enable(UartConfig::default(), clocks.peripheral_clock.freq())
+    let dc = pins.gpio16.into_push_pull_output();
+    let cs = pins.gpio17.into_push_pull_output();
+
+    // SPI interface
+    let spi_device = embedded_hal_bus::spi::ExclusiveDevice::new(spi, cs, timer).unwrap();
+    let di = display_interface_spi::SPIInterface::new(spi_device, dc);
+
+    // Reset pin must be set to high.
+    let mut rst = pins.gpio22.into_push_pull_output();
+    rst.set_high().unwrap();
+
+    // Configure the backlight pin, set to high
+    let mut bl = pins.gpio20.into_push_pull_output();
+    bl.set_high().unwrap();
+
+    rprintln!("Just before initializing display");
+
+    // Initialize the display
+    let mut display = Builder::new(models::ST7789, di)
+        .reset_pin(rst)
+        .display_size(WIDTH as u16, HEIGHT as u16)
+        .invert_colors(options::ColorInversion::Inverted)
+        .orientation(options::Orientation::default())
+        .color_order(options::ColorOrder::Bgr)
+        .init(&mut timer)
         .unwrap();
 
-    uart.write_full_blocking(b"Hello, World!\r\n");
+    rprintln!("Display initialized");
+
+    led_pin.set_high().unwrap();
+    // Clear the display initially
+    display.clear(Rgb565::GREEN).unwrap();
+
+    // Draw a rectangle on screen
+    let style = embedded_graphics::primitives::PrimitiveStyleBuilder::new()
+        .fill_color(Rgb565::GREEN)
+        .build();
+
+    embedded_graphics::primitives::Rectangle::new(Point::zero(), display.bounding_box().size)
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
 
     loop {
-        uart.write_full_blocking(b"Hello!\r\n");
-        led_pin.set_high().unwrap();
-        timer.delay_ms(500);
-        led_pin.set_low().unwrap();
-        timer.delay_ms(500);
+        // Do nothing
     }
 }
